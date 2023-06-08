364de733d0f42a34f5b54f558db9e59b2319f75b
Gunsnfnr <126002661+Gunsnfnr@users.noreply.github.com>
2023-06-08 22:48:12 +0300

    Update task1.ts

--- task1.ts
+++ task1.ts
@@ -11,13 +11,13 @@ class Job {
   };
   
   public work(personName: string): void {
-    console.log(`${personName} сейчас работает как ${this.role}`);
+    console.log(`${personName} сейчас работает как ${this.role}, его з/п = ${this.salary}`);
 }
-
+  changeJob(newJob: string, newSalary: number):void {
+    this.role = newJob;
+    this._salary = newSalary;
+  }
 }
-
-const employee1 = new Job('грузчик', 35_000);
-employee1.work('Иван Семенов');
 
 class Person {
   private job: Job;
@@ -33,9 +33,18 @@ class Person {
     this.job = job;
   };
   getToWork():void {
-    this.job.work('name');
+    this.job.work(this.name);
   }
 }
 
-const person1 = new Person('Алесей Жуков', employee1);
+const employee1 = new Job('грузчик', 35_000);
+const person1 = new Person('Алексей Жуков', employee1);
 person1.getToWork();
+employee1.changeJob('водитель погрузчика', 39_000);
+person1.getToWork();
+
+const employee2 = new Job('охранник', 31_000);
+const person2 = new Person('Геннадий Печенкин', employee2);
+person2.getToWork();
+employee2.changeJob('начальник охраны', 42_000);
+person2.getToWork();
