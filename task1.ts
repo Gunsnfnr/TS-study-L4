0c743f583a68de468258bad4fd57d50e64d70253
Gunsnfnr <126002661+Gunsnfnr@users.noreply.github.com>
2023-06-09 16:05:55 +0300

    Update task1.ts

--- task1.ts
+++ task1.ts
@@ -4,6 +4,9 @@ class Job {
     get salary(): number {
         return this._salary;
     };
+    set setSalary(_salary: number) {
+      this._salary = _salary;
+    }
 
   constructor(role: string, salary: number) {
     this.role = role;
@@ -13,10 +16,6 @@ class Job {
   public work(personName: string): void {
     console.log(`${personName} сейчас работает как ${this.role}, его з/п = ${this.salary}`);
 }
-  changeJob(newJob: string, newSalary: number):void {
-    this.role = newJob;
-    this._salary = newSalary;
-  }
 }
 
 class Person {
@@ -37,14 +36,18 @@ class Person {
   }
 }
 
-const employee1 = new Job('грузчик', 35_000);
-const person1 = new Person('Алексей Жуков', employee1);
-person1.getToWork();
-employee1.changeJob('водитель погрузчика', 39_000);
+const position1 = new Job('грузчик', 35_000);
+const person1 = new Person('Алексей Жуков', position1);
 person1.getToWork();
 
-const employee2 = new Job('охранник', 31_000);
-const person2 = new Person('Геннадий Печенкин', employee2);
+const position2 = new Job('водитель погрузчика', 39_000);
+person1.setJob = position2;
+person1.getToWork();
+
+const position3 = new Job('охранник', 31_000);
+const person2 = new Person('Геннадий Печенкин', position2);
 person2.getToWork();
-employee2.changeJob('начальник охраны', 42_000);
+
+const position4 = new Job('начальник охраны', 42_000);
+person2.setJob = position4;
 person2.getToWork();
